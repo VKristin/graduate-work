ec50ac563208d746922d54162d8f0467525f6126
VKristin <christinavoronova12@gmail.com>
2023-05-25 23:37:50 +0500

    25.05.2023

---
+++ Diagrams/VectorGraphics/StepsOfBlockSchema.Designer.cs
@@ -0,0 +1,194 @@
+﻿namespace Diagrams
+{
+    partial class StepsOfBlockSchema
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitter4 = new System.Windows.Forms.Splitter();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.splitter3 = new System.Windows.Forms.Splitter();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.splitter2 = new System.Windows.Forms.Splitter();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.dgvFirst = new System.Windows.Forms.DataGridView();
+            this.Action = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dgvSecond = new System.Windows.Forms.DataGridView();
+            this.dataGridViewTextBoxColumn1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dgvThird = new System.Windows.Forms.DataGridView();
+            this.dataGridViewTextBoxColumn2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.panel3.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvFirst)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvSecond)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvThird)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // splitter4
+            // 
+            this.splitter4.Location = new System.Drawing.Point(783, 0);
+            this.splitter4.Name = "splitter4";
+            this.splitter4.Size = new System.Drawing.Size(3, 318);
+            this.splitter4.TabIndex = 28;
+            this.splitter4.TabStop = false;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.panel3.Controls.Add(this.dgvThird);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel3.Location = new System.Drawing.Point(524, 0);
+            this.panel3.Name = "panel3";
+            this.panel3.Padding = new System.Windows.Forms.Padding(3);
+            this.panel3.Size = new System.Drawing.Size(259, 318);
+            this.panel3.TabIndex = 24;
+            // 
+            // splitter3
+            // 
+            this.splitter3.Location = new System.Drawing.Point(521, 0);
+            this.splitter3.Name = "splitter3";
+            this.splitter3.Size = new System.Drawing.Size(3, 318);
+            this.splitter3.TabIndex = 27;
+            this.splitter3.TabStop = false;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.panel2.Controls.Add(this.dgvSecond);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel2.Location = new System.Drawing.Point(262, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Padding = new System.Windows.Forms.Padding(3);
+            this.panel2.Size = new System.Drawing.Size(259, 318);
+            this.panel2.TabIndex = 25;
+            // 
+            // splitter2
+            // 
+            this.splitter2.Location = new System.Drawing.Point(259, 0);
+            this.splitter2.Name = "splitter2";
+            this.splitter2.Size = new System.Drawing.Size(3, 318);
+            this.splitter2.TabIndex = 26;
+            this.splitter2.TabStop = false;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.Highlight;
+            this.panel1.Controls.Add(this.dgvFirst);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Padding = new System.Windows.Forms.Padding(3);
+            this.panel1.Size = new System.Drawing.Size(259, 318);
+            this.panel1.TabIndex = 23;
+            // 
+            // dgvFirst
+            // 
+            this.dgvFirst.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvFirst.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Action});
+            this.dgvFirst.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgvFirst.Location = new System.Drawing.Point(3, 3);
+            this.dgvFirst.Name = "dgvFirst";
+            this.dgvFirst.Size = new System.Drawing.Size(253, 312);
+            this.dgvFirst.TabIndex = 0;
+            // 
+            // Action
+            // 
+            this.Action.HeaderText = "Действие";
+            this.Action.Name = "Action";
+            // 
+            // dgvSecond
+            // 
+            this.dgvSecond.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvSecond.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.dataGridViewTextBoxColumn1});
+            this.dgvSecond.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgvSecond.Location = new System.Drawing.Point(3, 3);
+            this.dgvSecond.Name = "dgvSecond";
+            this.dgvSecond.Size = new System.Drawing.Size(253, 312);
+            this.dgvSecond.TabIndex = 1;
+            // 
+            // dataGridViewTextBoxColumn1
+            // 
+            this.dataGridViewTextBoxColumn1.HeaderText = "Действие";
+            this.dataGridViewTextBoxColumn1.Name = "dataGridViewTextBoxColumn1";
+            // 
+            // dgvThird
+            // 
+            this.dgvThird.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvThird.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.dataGridViewTextBoxColumn2});
+            this.dgvThird.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgvThird.Location = new System.Drawing.Point(3, 3);
+            this.dgvThird.Name = "dgvThird";
+            this.dgvThird.Size = new System.Drawing.Size(253, 312);
+            this.dgvThird.TabIndex = 1;
+            // 
+            // dataGridViewTextBoxColumn2
+            // 
+            this.dataGridViewTextBoxColumn2.HeaderText = "Действие";
+            this.dataGridViewTextBoxColumn2.Name = "dataGridViewTextBoxColumn2";
+            // 
+            // StepsOfBlockSchema
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(828, 318);
+            this.Controls.Add(this.splitter4);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.splitter3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.splitter2);
+            this.Controls.Add(this.panel1);
+            this.Name = "StepsOfBlockSchema";
+            this.Text = "StepsOfBlockSchema";
+            this.panel3.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvFirst)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvSecond)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvThird)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Splitter splitter4;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.DataGridView dgvThird;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn2;
+        private System.Windows.Forms.Splitter splitter3;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.DataGridView dgvSecond;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn1;
+        private System.Windows.Forms.Splitter splitter2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.DataGridView dgvFirst;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Action;
+    }
+}
